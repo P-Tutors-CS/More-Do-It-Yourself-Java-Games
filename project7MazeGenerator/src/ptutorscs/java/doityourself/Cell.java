483e9831679470d8246c0f64d305da0c1f1330d6
TPauline <73195686+TPauline@users.noreply.github.com>
2024-08-07 16:19:08 -0400

    project7MazeGenerator

--- project7MazeGenerator/src/ptutorscs/java/doityourself/Cell.java
+++ project7MazeGenerator/src/ptutorscs/java/doityourself/Cell.java
@@ -1,5 +1,6 @@
 package ptutorscs.java.doityourself;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import javax.swing.JPanel;
@@ -24,6 +25,28 @@ public class Cell extends JPanel {
     private static final int SIZE = 20;
 
     /*
+     * Each cell must have a row and a column, and walls on any
+     * number of its four sides.
+     * 
+     * 1. Add four public static final integer instance variables
+     * called TOP, RIGHT, BOTTOM, and LEFT, set to O, 1, 2, and
+     * 3, respectively.
+     * 2. Add two private integer instance variables called row
+     * and col, each initialized to -1.
+     * 3. Add a private boolean array instance variable called
+     * wall, with four elements, all with value true.
+     */
+    public static final int TOP = 0;
+    public static final int RIGHT = 1;
+    public static final int BOTTOM = 2;
+    public static final int LEFT = 3;
+
+    private int row = -1;
+    private int col = -1;
+
+    private boolean[] wall = { true, true, true, true };
+
+    /*
      * To be able to draw on a panel, you must provide a method
      * called paintComponent(), with one parameter, a Graphics
      * object, and returns nothing.
@@ -41,11 +64,76 @@ public class Cell extends JPanel {
      * 2. In paintComponent(), draw a line ong from coordinates
      * O and O to coordinates SIZE and SIZE. (Hint: use
      * Graphics' drawLine() method.)
+     * 
+     * Try It - Draw a Rectangle on a Panel
+     * 
+     * Drawing a rectangle is similar to drawing a line: use
+     * drawRect() with the location of the top left corner of the
+     * rectangle and the width and height of the rectangle. To draw
+     * a rectangle at O, 5, 10 pixels wide, and 20 pixels tall, use:
+     * g.drawRect(O, 5, 10, 20)
+     * 
+     * In what color would the rectangle be drawn? The default is
+     * black. But ifyou want to use a different color, first switch the
+     * color with Graphics' setColor() method.
+     * 
+     * You can also draw a filled rectangle using a method similar to
+     * drawRect():
+     *
+     * g.fi11Rect(O, 5, 10, 20)
+     * 
+     * Try out these new methods by drawing a solid white square
+     * that's the same size as Cell and outlined in black.
+     * 
+     * 1. Remove the code in paintComponent().
+     * 
+     * 2. Set the color for g to white. (Hint: use Graphics' setColor()
+     * method.)
+     * 
+     * 3. Draw a filled rectangle on g that fills the entire cell.
+     * (Hint: use Graphics' fillRect() method to fill a rectangle
+     * starting at O, O, and SIZE wide and SIZE tall.)
+     * 
+     * 4. Set the color for g to black. (Hint: use Graphics' setColor()
+     * method.)
+     * 
+     * Try It - Draw a Circle
+     * Another shape you'll need to be able to draw for this game
+     * is a circle. Before drawing a circle, imagine a rectangle that
+     * surrounds your circle. To draw the circle, use drawOvaI()
+     * with the position, width and height of that rectangle. For
+     * example, to draw an oval that would fit inside a rectangle at
+     * O, 10, width 30, and height 15, use:
+     * 
+     * g.drawOval(O, 10, 30, 15);
+     * 
+     * Like Java provides both drawRect() andfillRect() methods, so
+     * too does it provide both drawOval() andfillOval() methods,
+     * with the latter drawing a filled-in circle or oval.
+     * 
+     * Add code to paintComponent() to draw a smaller green circle
+     * inside the square.
+     * 
+     * 1. Set the color for g to green. (Hint: use Graphics' setColor()
+     * method.)
+     * 
+     * 2. Draw a filled circle on g centered in the cell, 6 pixels
+     * smaller than the cell. (Hint: use Graphics' fillOval()
+     * method. Hint: if the circle is 6 pixels smaller than the
+     * square, where would you have to start drawing the
+     * circle so the circle appears centered in the square? Hint:
+     * Calculate this number from SIZE.)
      */
 
     @Override
     public void paintComponent(Graphics g) {
-        g.drawLine(0, 0, SIZE, SIZE);
+        // g.drawLine(0, 0, SIZE, SIZE);
+        g.setColor(Color.WHITE);
+        g.fillRect(0, 0, SIZE, SIZE);
+        g.setColor(Color.BLACK);
+        g.fillRect(0, 0, SIZE, SIZE);
+        g.setColor(Color.GREEN);
+        g.fillOval(3, 3, SIZE - 6, SIZE - 6);
 
     }
 
@@ -65,8 +153,8 @@ public class Cell extends JPanel {
      * 4. Return size.
      */
     @Override
-    public Dimension getPreferredSize(){
-        Dimension size = new Dimension(SIZE,SIZE);
+    public Dimension getPreferredSize() {
+        Dimension size = new Dimension(SIZE, SIZE);
         return size;
     }
 }
