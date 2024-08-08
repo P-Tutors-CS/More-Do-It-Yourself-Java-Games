ff098b8b3c27ec3580b755ac660e1f016082a3ab
TPauline <73195686+TPauline@users.noreply.github.com>
2024-08-07 20:45:10 -0400

    project7MazeGenerator

--- project7MazeGenerator/src/ptutorscs/java/doityourself/Cell.java
+++ project7MazeGenerator/src/ptutorscs/java/doityourself/Cell.java
@@ -45,95 +45,147 @@ public class Cell extends JPanel {
     private int col = -1;
 
     private boolean[] wall = { true, true, true, true };
+    private boolean[] path = { false, false, false, false };
+
+    private boolean current = false;
+    private boolean end = false;
+
+    /*
+     * Initialize the instance variables when the cell is constructed.
+     * 
+     * 1. Add a Cell constructor with two parameters, integers
+     * called row and col.
+     * 
+     * 2. Set the instance variables row and col to the row and col
+     * parameter values. (Hint: use this.)
+     * 
+     * The cell will need some public methods so MazeGenerator
+     * can access the private instance variables.
+     * 
+     * 1. Add public methods called getRow() and getCol() that
+     * return integers, the values of row and col, respectively.
+     * 
+     * 2. Add a public method called isWall(). It should take one
+     * parameter, an integer called index, and return a boolean,
+     * the value of the element in wall at index.
+     */
+    public Cell(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public boolean isWall(int index) {
+        return wall[index];
+    }
 
     /*
      * To be able to draw on a panel, you must provide a method
      * called paintComponent(), with one parameter, a Graphics
      * object, and returns nothing.
-     *
-     * Graphics has many methods that can set colors, draw shapes,
-     * and much more. For example, to draw a line from position O,
-     * 5 to position 10, 20 on a Graphics object called g, use:
-     * g.drawLine(O, 5, 10, 20);
      * 
-     * 1. Add a new public method to Cell called
-     * paintComponent(). paintComponent() should have one
-     * parameter, a Graphics object called g, and return
-     * nothing.
+     * Cells are created with all four walls present. Later,
+     * MazeGenerator will remove walls as it generates the maze.
+     * paintComponent() will be called many times while running
+     * the program, including after some walls have been removed.
+     * Therefore, paintComponent() will need to draw lines only for
+     * the walls of the cell that are still present.
      * 
-     * 2. In paintComponent(), draw a line ong from coordinates
-     * O and O to coordinates SIZE and SIZE. (Hint: use
-     * Graphics' drawLine() method.)
+     * 1. In the draw the walls section of paintComponent(), if the
+     * top wall is present, draw a line at the top of the cell.
+     * (Hint: Is the TOP element in wall true? Hint: draw a line
+     * starting at the top left corner of the cell, O, O. Should you
+     * change x or y to go to the right? Draw the line the length
+     * of SIZE.)
      * 
-     * Try It - Draw a Rectangle on a Panel
+     * 2. Ifthe left side wall is present, draw a line at the left edge
+     * of the cell. (Hint: is the LEFT element in wall true? Hint:
+     * draw a line starting at the top left corner of the cell,
+     * 0,0. Should you change x or y to draw a downward line?
+     * Draw the line the length of SIZE.)
      * 
-     * Drawing a rectangle is similar to drawing a line: use
-     * drawRect() with the location of the top left corner of the
-     * rectangle and the width and height of the rectangle. To draw
-     * a rectangle at O, 5, 10 pixels wide, and 20 pixels tall, use:
-     * g.drawRect(O, 5, 10, 20)
-     * 
-     * In what color would the rectangle be drawn? The default is
-     * black. But ifyou want to use a different color, first switch the
-     * color with Graphics' setColor() method.
-     * 
-     * You can also draw a filled rectangle using a method similar to
-     * drawRect():
-     *
-     * g.fi11Rect(O, 5, 10, 20)
-     * 
-     * Try out these new methods by drawing a solid white square
-     * that's the same size as Cell and outlined in black.
-     * 
-     * 1. Remove the code in paintComponent().
-     * 
-     * 2. Set the color for g to white. (Hint: use Graphics' setColor()
-     * method.)
-     * 
-     * 3. Draw a filled rectangle on g that fills the entire cell.
-     * (Hint: use Graphics' fillRect() method to fill a rectangle
-     * starting at O, O, and SIZE wide and SIZE tall.)
-     * 
-     * 4. Set the color for g to black. (Hint: use Graphics' setColor()
-     * method.)
-     * 
-     * Try It - Draw a Circle
-     * Another shape you'll need to be able to draw for this game
-     * is a circle. Before drawing a circle, imagine a rectangle that
-     * surrounds your circle. To draw the circle, use drawOvaI()
-     * with the position, width and height of that rectangle. For
-     * example, to draw an oval that would fit inside a rectangle at
-     * O, 10, width 30, and height 15, use:
-     * 
-     * g.drawOval(O, 10, 30, 15);
-     * 
-     * Like Java provides both drawRect() andfillRect() methods, so
-     * too does it provide both drawOval() andfillOval() methods,
-     * with the latter drawing a filled-in circle or oval.
-     * 
-     * Add code to paintComponent() to draw a smaller green circle
-     * inside the square.
-     * 
-     * 1. Set the color for g to green. (Hint: use Graphics' setColor()
-     * method.)
-     * 
-     * 2. Draw a filled circle on g centered in the cell, 6 pixels
+     * Draw a green ball ifthis cell is the current cell and a red ball if
+     * this cell is the end cell.
+     * 1. In the draw the balls section of paintComponent, if
+     * current is true:
+     * a. Set the color of g to green. (Hint: use Graphics'
+     * setColor() method.)
+     * b. Draw a filled circle on g centered in the cell, 6 pixels
      * smaller than the cell. (Hint: use Graphics' fillOval()
-     * method. Hint: if the circle is 6 pixels smaller than the
-     * square, where would you have to start drawing the
-     * circle so the circle appears centered in the square? Hint:
-     * Calculate this number from SIZE.)
+     * method. Hint: if the circle is 6 pixels smaller than
+     * the square, where would you have to start drawing
+     * the circle so the circle appears centered in the
+     * square?)
+     * 2. Otherwise, if end is true:
+     * a. Set the color of g to red. (Hint: use Graphics'
+     * setColor() method.)
+     * b. Draw a filled circle on g centered in the cell, 6 pixels
+     * smaller than the cell. (Hint: draw the red ball the
+     * same size as the green ball.)
+     * 
+     * Next, draw the paths that have been taken through this cell.
+     * 1. In the draw the path section of paintComponent(), set the
+     * color Ofg to green.
+     * 2. If the following path is true, draw the corresponding
+     * line. (Hint: use Graphics' drawLine() method. Hint:
+     * calculate the middle and center of the cell using SIZE.)
+     * a. If the TOP element in path is true, draw a line on g
+     * from the middle top edge of the cell to the center of
+     * the cell.
+     * b. If the BOTTOM element in path is true, draw a line
+     * on g from the middle bottom edge of the cell to the
+     * center of the cell.
+     * c. If the LEFT element in path is true, draw a line on g
+     * from the middle left edge of the cell to the center of
+     * the cell.
+     * d. If the RIGHT element in path is true, draw a line on
+     * g from the middle right edge of the cell to the center
+     * of the cell.
+     * 
      */
-
     @Override
     public void paintComponent(Graphics g) {
-        // g.drawLine(0, 0, SIZE, SIZE);
+        // draw backgorund
         g.setColor(Color.WHITE);
         g.fillRect(0, 0, SIZE, SIZE);
         g.setColor(Color.BLACK);
-        g.fillRect(0, 0, SIZE, SIZE);
+
+        // draw the walls
+        if (isWall(TOP)) {
+            g.drawLine(0, 0, SIZE, 0);
+        }
+        if (isWall(LEFT)) {
+            g.drawLine(0, 0, 0, SIZE);
+        }
+        // draw the path
         g.setColor(Color.GREEN);
-        g.fillOval(3, 3, SIZE - 6, SIZE - 6);
+        if(path[TOP]){
+            g.drawLine(SIZE/2, 0, SIZE / 2, SIZE / 2);
+        }
+        if (path[BOTTOM]) {
+            g.drawLine(SIZE / 2, SIZE, SIZE / 2, SIZE / 2);
+        }
+        if (path[LEFT]) {
+            g.drawLine(0, SIZE/2, SIZE / 2, SIZE / 2);
+        }
+        if (path[RIGHT]) {
+            g.drawLine(SIZE, SIZE/2, SIZE / 2, SIZE / 2);
+        }
+        // draw the balls
+        if (current) {
+            g.setColor(Color.GREEN);
+            g.fillOval(3, 3, SIZE - 6, SIZE - 6);
+        } else if (end) {
+            g.setColor(Color.RED);
+            g.fillOval(3, 3, SIZE - 6, SIZE - 6);
+        }
 
     }
 
@@ -156,5 +208,114 @@ public class Cell extends JPanel {
     public Dimension getPreferredSize() {
         Dimension size = new Dimension(SIZE, SIZE);
         return size;
+    }
+
+    /*
+     * 2. Add a public method called hasAllWalls(). It should take
+     * no parameters and return a boolean.
+     * 3. Add a boolean called allWalls which is true if every
+     * element in wall is true. (Hint: use logical and, &&, to
+     * compare the contents of each element.)
+     * 4. Return allWalls.
+     */
+    public boolean hasAllWalls() {
+        boolean allWalls = wall[0] && wall[1] && wall[2] && wall[3];
+        return allWalls;
+    }
+
+    /*
+     * And MazeGenerator will need to be able to remove a wall
+     * from the cell.
+     * 1. Add a public method called removeWall(). It should take
+     * one parameter, an integer called w, and return nothing.
+     * 2. Set the element at w in wall to false.
+     * 3. Repaint the cell. (Hint: call JPanel's repaint() method.)
+     */
+    public void removeWall(int w) {
+        wall[w] = false;
+        repaint();
+    }
+
+    /*
+     * MazeGenerator will need a way to make an opening from one
+     * cell to the next. A method will be needed to remove the wall
+     * between the cells. Note, this involves removing walls in two
+     * cells, because each cell is initially created with a wall on each
+     * of its 4 sides..
+     * 1. Add a public method called openTo(). It should take
+     * one parameter, a Cell called neighbor and should return
+     * nothing.
+     * 2. If row is less than the neighbor's row, remove the bottom
+     * wall of this cell and the top wall of neighbor. (Hint, use
+     * getRow() to get the row for neighbor. Use removeWall() to
+     * remove a wall of this cell and remove a wall of neighbor.
+     * Hint: use TOP, BOTTOM, LEFT, or RIGHT as needed.)
+     * 3. Otherwise, if row is greater than the neighbor's row,
+     * remove the top wall of this cell and the bottom wall
+     * of neighbor. (Hint, use getRow(), removeWall(), and TOP,
+     * BOTTOM, LEFT, or RIGHT as needed.)
+     * 4. Otherwise, if col is less than the neighbor's column,
+     * remove the right wall of this cell and the left wall
+     * of neighbor. (Hint, use getCol(), removeWall(), and TOP,
+     * BOTTOM, LEFT, or RIGHT as needed.)
+     * 5. Otherwise, if col is greater than the neighbor's column,
+     * remove the left wall of this cell and the right wall
+     * Of neighbor. (Hint, use getCoI(), removeWaII(), and TOP,
+     * BOTTOM, LEFT, or RIGHT as needed.)F
+     */
+    public void openTo(Cell neighbor) {
+        if (row < neighbor.getRow()) {
+            removeWall(BOTTOM);
+            neighbor.removeWall(TOP);
+        } else if (row > neighbor.getRow()) {
+            removeWall(TOP);
+            neighbor.removeWall(BOTTOM);
+        } else if (col < neighbor.getCol()) {
+            removeWall(RIGHT);
+            neighbor.removeWall(LEFT);
+        } else if (col > neighbor.getCol()) {
+            removeWall(LEFT);
+            neighbor.removeWall(RIGHT);
+        }
+    }
+
+    /*
+     * MazeGenerator will need a way to make the cell current.
+     * 1. Add a public method called setCurrent(). It should take
+     * one parameter, a boolean called current, and return
+     * nothing.
+     * 2. Set the instance variable current to the current
+     * parameter value. (Hint: use this.)
+     * 3. Repaint the cell. (Hint: use JPanels repaint() method.)
+     */
+    public void setCurrent(boolean current) {
+        this.current = current;
+        repaint();
+    }
+
+    /*
+     * And MazeGenerator will need a way to make the cell the end
+     * cell.
+     * 1. Add a public method called setEnd(). It should take one
+     * parameter, a boolean called end, and return nothing.
+     * 2. Set the instance variable end to the end parameter value.
+     * (Hint: use this.)
+     * 3. Repaint the cell. (Hint: use JPanels repaint() method.)
+     */
+    public void setEnd(boolean end) {
+        this.end = end;
+    }
+
+    /*
+     * MazeGenerator will need a way to update the path taken
+     * through the cell.
+     * 1. Add a public method called addPath(). It should take one
+     * parameter, an integer called side, and return nothing.
+     * 2. Set the path element at side to true.
+     * 3. Repaint the cell. (Hint: use JPanels repaint() method.)
+     */
+    public void addPath(int side) {
+        path[side] = true;
+        repaint();
     }
 }
--- project7MazeGenerator/src/ptutorscs/java/doityourself/MazeGenerator.java
+++ project7MazeGenerator/src/ptutorscs/java/doityourself/MazeGenerator.java
@@ -1,9 +1,17 @@
 package ptutorscs.java.doityourself;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.EventQueue;
+import java.awt.GridLayout;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import java.util.Random;
+
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.UIManager;
 
 /* 
@@ -37,6 +45,41 @@ public class MazeGenerator extends JFrame {
     private static final long serialVersionUID = 1L;
     private JLabel titLabel = new JLabel("Maze Generator");
 
+    /*
+     * Try It - Put a Grid of Cells with
+     * Walls in the Window
+     *
+     * update MazeGenerator to now add a grid of cells with 5 rows and 5 columns
+     * 
+     * 1. Go back to MazeGenerator.java.
+     * 2. Add private integer instance variables called rows and
+     * cols, each initialized to 5.
+     * 3. Create a private Cell two-dimensional array instance
+     * variable called cell, with rows rows and cols columns.
+     * 4. Create a private JPanel instance variable called
+     * mazePanel, initialized by calling JPanel's constructor.
+     */
+    private int rows = 5;
+    private int cols = 5;
+    Cell[][] cell = new Cell[rows][cols];
+    private JPanel mazePanel = new JPanel();
+
+    /*
+     * MazeGenerator still doesn't paint any balls. That's because
+     * MazeGenerator hasn't yet set the current and end positions
+     * of the maze. First create the variables to hold the values.
+     * 1. Go back to MazeGenerator.java.
+     * 2. Add two private integer instance variables called row
+     * and col, each initialized to O.
+     * 3. Add two private integer instance variables called
+     * endRow and endCol, initialized to one less than the
+     * values of rows and cols, respectively.
+     */
+    private int row = 0;
+    private int col = 0;
+    private int endRow = rows - 1;
+    private int endCol = cols - 1;
+
     public MazeGenerator() {
         initGUI();
 
@@ -47,23 +90,195 @@ public class MazeGenerator extends JFrame {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 
+    /*
+     * 1. In the center panel section Of initGUI(), create a JPaneI
+     * object called centerPanel.
+     * 2. Set the background color of centerPanel to black. (Hint:
+     * use JPanel's setBackground() method.)
+     * 3. Add centerPanel to the center of the window. (Hint:
+     * use JFrame's add() method and BorderLayout's CENTER
+     * variable.)
+     * 4. In the maze panel section of initGUI(), call newMaze().
+     * 5. Add mazePanel to centerPanel. (Hint: use JPanel's add()
+     * method.)
+     * 
+     * Try It - Listen for Keystrokes
+     * Next you'll add code to move the green ball through the
+     * maze.
+     * To make MazeGenerator listen for keystrokes from the
+     * keyboard, add a key listener. Adding key listeners is very
+     * similar to adding action listeners.
+     * Instead of adding the key listener to a button, you'll add the
+     * key listener to the window itself (the JFrame).
+     * 1. In the listeners section of initGUI(), add a key listener to
+     * the window by inserting code exactly as shown.
+     */
     private void initGUI() {
         add(titLabel, BorderLayout.PAGE_START);
 
-        /*
-         * Try It - Add a Cell to the Window
-         * Next, add a Cell to the window.
-         * 1. Go back to MazeGenerator.java.
-         * 
-         * 2. In initGUI(), create a new Cell object called cell,
-         * initialized by calling Cell's constructor.
-         * 
-         * 3. Add cell to the center of the window. (Hint: JFrame's
-         * add() method and BorderLayout's CENTER variable.)
-         */
+        // center panel
+        JPanel centerPanel = new JPanel();
+        centerPanel.setBackground(Color.BLACK);
 
-         Cell cell = new Cell();
-         add(cell,BorderLayout.CENTER);
+        add(centerPanel, BorderLayout.CENTER);
+
+        // maze panel
+        newMaze();
+        centerPanel.add(mazePanel);
+
+        // button panel
+
+        // listeners
+        addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyReleased(KeyEvent e){
+                int keyCode = e.getKeyCode();
+                moveBall(keyCode);
+            }
+        });
+    }
+
+    /*
+     * 1. Add a private method called newMaze(). It should take
+     * no parameters and return nothing.
+     * 2. Set the layout for mazePanel to a new GridLayout, rows
+     * rows and cols columns. (Hint: use JPane1's setLayout()
+     * method and the GridLayout constructor that takes two
+     * integers.)
+     * 3. Set cell to be a new Cell two-dimensional array, with rows
+     * rows and cols columns.
+     * 4. Use a double for loop to go through each element in cell,
+     * using integers r and c as the iteration variables for the
+     * for loops:
+     * a. Create a new cell in cell in at r and c. (Hint: use r and
+     * c as parameter values for the Cell constructor.)
+     * b. Add the cell in cell at r and c to mazePanel. (Hint: use
+     * JPanel's add() method.)
+     * 
+     * 1. Call generateMaze() from the end of newMaze().
+     * 
+     * When a new maze is created, reset the current and end row
+     * and column variables to their initial values.
+     * 1. Add code to the end of newMaze() to set row and col to O.
+     * 2. Set endRow and endCol to one less than the values of rows
+     * and cots, respectively.
+     * 3. Set the cell at row and col to the current cell. (Hint: use
+     * Cell's setCurrent() method.)
+     * 4. Set the cell at endRow and endCol to be the end cell. (Hint:
+     * use Cell's setEnd() method.)
+     */
+    public void newMaze() {
+        mazePanel.setLayout(new GridLayout(rows, cols));
+        cell = new Cell[rows][cols];
+
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                cell[r][c] = new Cell(r, c);
+                mazePanel.add(cell[r][c]);
+            }
+        }
+        generateMaze();
+
+        row = 0;
+        col = 0;
+        endRow = rows - 1;
+        endCol = cols - 1;
+        cell[row][col].setCurrent(true);
+        cell[endRow][endCol].setEnd(true);
+    }
+
+    /*
+     * Try It - Remove Some Walls to Form a Maze
+     * MazeGenerator needs a way to check whether a cell is
+     * available to be added to the maze, in other words, if the cell
+     * still has all four walls. However, MazeGenerator must first
+     * check whether the given row and column are in the maze.
+     * 1. Go back to MazeGenerator.java.
+     * 2. Create a private method called isAvailable(). It should
+     * take two parameters, integers called r and c, and return a
+     * boolean.
+     * 3. Create a boolean called available, initialized tofalse.
+     * 4. If r and c contain valid row and column values, and if cell
+     * at r and c has all four walls, set available to true. (Hint:
+     * r and c must be greater than or equal to O and less than
+     * the number of rows and cols, respectively. Hint: use Cell's
+     * hasAllWalls() method.)
+     * 5. Return available.
+     */
+    public boolean isAvailable(int r, int c) {
+        boolean available = false;
+        if (r >= 0 && r < rows && c >= 0 && c < cols && cell[r][c].hasAllWalls()) {
+            available = true;
+        }
+        return available;
+    }
+
+    /*
+     * How do you generate a maze with only one way to reach any
+     * cell and with no unreachable cells?
+     * • Start at a random cell.
+     * • While not all cells have yet been visited,
+     * - Find all neighbors with all walls intact.
+     * - If one or more was found:
+     * ~ If more than one was found, add that cell to the
+     * list to try later.
+     * ~ Pick a neighbor and remove the adjacent wall.
+     * ~ Go to the neighbor and increment the number
+     * visited.
+     * • If none was found, go to one of the cells saved in the
+     * list to try later.
+     */
+    public void generateMaze() {
+        ArrayList<Cell> tryLaterCell = new ArrayList<>();
+        int totalCells = rows * cols;
+        int visitesCells = 1;
+
+        // start at a random cell
+        Random rand = new Random();
+        int r = rand.nextInt(rows);
+        int c = rand.nextInt(cols);
+
+        // while not all cells have yet been visited
+        while (visitesCells < totalCells) {
+
+            // find all neighbors with all walls intact
+            ArrayList<Cell> neighbors = new ArrayList<>();
+            if (isAvailable(r - 1, c)) {
+                neighbors.add(cell[r - 1][c]);
+            }
+            if (isAvailable(r + 1, c)) {
+                neighbors.add(cell[r + 1][c]);
+            }
+            if (isAvailable(r, c - 1)) {
+                neighbors.add(cell[r][c - 1]);
+            }
+            if (isAvailable(r, c + 1)) {
+                neighbors.add(cell[r][c + 1]);
+            }
+
+            // if one or more found
+            if (neighbors.size() > 0) {
+                // if more than one found, add this cell to the list to try again
+                if (neighbors.size() > 1) {
+                    tryLaterCell.add(cell[r][c]);
+                }
+
+                // pick a neighbor and remove the wall
+                int pick = rand.nextInt(neighbors.size());
+                Cell neighbor = neighbors.get(pick);
+                cell[r][c].openTo(neighbor);
+
+                // go to the neighbor and increment the number visited
+                r = neighbor.getRow();
+                c = neighbor.getCol();
+                visitesCells++;
+            } else {
+                // if none was found, go to one of the cells that was saved to try later
+                Cell nextCell = tryLaterCell.remove(0);
+                r = nextCell.getRow();
+                c = nextCell.getCol();
+            }
+        }
     }
 
     public static void main(String[] args) {
